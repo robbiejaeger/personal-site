43b6509c9caa854f8d188bf0d3f3f64c276e22ab
Robbie Jaeger <jaeger.rob@gmail.com>
2017-11-14 22:39:30 -0500

    Move http redirect up in server file

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(`${__dirname}/public`));
-app.set('port', process.env.PORT || 3000);
-
 const requireHTTPS = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
+  if (req.headers['x-forwarded-proto'] != 'https') {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
 };
 
 app.use(requireHTTPS);
+app.use(express.static(`${__dirname}/public`));
+app.set('port', process.env.PORT || 3000);
+
 
 app.get('/', (req, res) => {
   app.sendFile('index.html');
